a6dd911ff41a6f89487761feca7ccc385fef7306
nosteiner <37960334+nosteiner@users.noreply.github.com>
2018-11-30 01:41:04 +0200

    first commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+   [SerializeField] float screenWidthInUnits = 16f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+	}
+}
