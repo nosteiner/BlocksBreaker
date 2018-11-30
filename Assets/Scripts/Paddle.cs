a35dc57a171b4e431c44d84dca424ec09cdf7d9c
Noam Steiner <no.steiner@gmail.com>
2018-11-30 15:54:22 +0200

    added paddle script

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
-   [SerializeField] float screenWidthInUnits = 16f;
+
+    [SerializeField] float minX = 1.22f;
+    [SerializeField] float maxX = 14.78f;
+    [SerializeField] float screenWidthInUnits = 16f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,9 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
 	}
 }
