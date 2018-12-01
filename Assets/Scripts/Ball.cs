7d7bb719463ca7df9987d01aaccaa69feceac996
Noam Steiner <no.steiner@gmail.com>
2018-12-01 15:46:10 +0200

    added coliders to the play space, changed gravity, stick the ball to the padlle + gave it velocity

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,16 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+    bool hasStarted = false;
+
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    //state
+    Vector2 paddleToBallVector;
 
 	// Use this for initialization
 	void Start () {
-		
+        paddleToBallVector = transform.position - paddle1.transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (hasStarted == false)
+        {
+            LockBallToPadlle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPadlle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
 }
