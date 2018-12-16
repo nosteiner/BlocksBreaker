7d708b655cb99d113b33b9afa7a0e764e61374ce
Noam Steiner <no.steiner@gmail.com>
2018-12-16 20:05:42 +0200

    added damage levels

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,26 +1,69 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour {
+    [SerializeField] int maxHits;
+    [SerializeField] int timesHit;
+    [SerializeField] Sprite[] hitSprites;
 
      Level level;
 
     private void Start()
     {
+        CountBreakableBlocks();
+
+    }
+
+    private void CountBreakableBlocks()
+    {
         level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        if (tag == "BreakableBlock")
+        {
+            level.CountBlocks();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if (tag == "BreakableBlock")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHit = hitSprites.Length + 1;
+        if (maxHits <= timesHit)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from array" + gameObject.name);
+        }
     }
 
     private void DestroyBlock()
     {
-        Destroy(gameObject);
-        level.BlockDestroyed();
-        FindObjectOfType<GameSession>().AddToScore();
+            Destroy(gameObject);
+            level.BlockDestroyed();
+            FindObjectOfType<GameSession>().AddToScore();
     }
 }
