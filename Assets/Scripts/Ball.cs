c730d7e95f1c85508979618cc921f59fd15c25a0
Noam Steiner <no.steiner@gmail.com>
2018-12-21 02:00:37 +0200

    added sound, need to fix ball speed

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,18 +8,21 @@ public class Ball : MonoBehaviour {
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
     [SerializeField] float randomFactor = 0.2f;
+    [SerializeField] AudioClip[] ballSounds;
 
     Rigidbody2D myRigidbody2D;
 
     //state
     Vector2 paddleToBallVector;
     Rigidbody2D myRigidBody2D;
+    AudioSource audioSource;
     
     // Use this for initialization
     void Start () {
         paddleToBallVector = transform.position - paddle1.transform.position;
         myRigidBody2D = GetComponent<Rigidbody2D>();
         myRigidBody2D.simulated = false;
+        audioSource = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
@@ -53,13 +56,26 @@ public class Ball : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+      
+
         Vector2 velocityTweak = new Vector2(
             Random.Range(0f, randomFactor),
             Random.Range(0f, randomFactor));
 
         if (hasStarted)
         {
+            if (collision.gameObject.tag == "BreakableBlock")
+            {
+                AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
+                audioSource.PlayOneShot(clip);
+            }
             myRigidBody2D.velocity += velocityTweak;
-        }
+
+            var magnitude = myRigidBody2D.velocity.magnitude;
+            myRigidBody2D.velocity += velocityTweak;
+            myRigidBody2D.velocity = myRigidBody2D.velocity.normalized;
+            myRigidBody2D.velocity *= magnitude;
+            //myRigidBody2D.velocity *= magnitude*1.001f;
+        }   
     }
 }
